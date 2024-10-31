26799be02fe309447c2e72eadadd8734fa956c5d
KaiChen1008 <1038790@gmail.com>
2024-10-31 19:11:53 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="auth",
+    name="auth-cmd",
     version="0.2",
     packages=find_packages(),
     include_package_data=True,
@@ -21,11 +21,14 @@ setup(
         ],
     },
     author="Ken Lin",
-    description="A TOTP authentication tool",
+    description="A 2FA/TOTP authentication tool",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/KaiChen1008/auth-cmd",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
